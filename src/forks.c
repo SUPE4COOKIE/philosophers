26f3f18cd480f6ca1fffa1c96e96ab80f8e987d4
supe4cookie <supe4cookie@gmail.com>
2024-09-01 03:11:22 +0200

    fixed helgrind

--- src/forks.c
+++ src/forks.c
@@ -6,7 +6,7 @@
 /*   By: mwojtasi <mwojtasi@student.42lyon.fr >     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 02:00:53 by mwojtasi          #+#    #+#             */
-/*   Updated: 2024/08/31 20:34:43 by mwojtasi         ###   ########.fr       */
+/*   Updated: 2024/09/01 03:10:30 by mwojtasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,11 +64,10 @@ int	take_forks(t_philo *philo)
 	count = philo->data->philo_count;
 	if (philo->data->philo_count == 1)
 		return (single_fork(philo));
-	forks_1st_half(forks, id, count);
-	//if (((id + 1) % count) > id)
-	//	forks_1st_half(forks, id, count);
-	//else
-	//	forks_2nd_half(forks, id, count);
+	if (((id + 1) % count) > id)
+		forks_1st_half(forks, id, count);
+	else
+		forks_2nd_half(forks, id, count);
 	return (print_forks_taken(philo));
 }
 
